964163f5bf8c9857aa5087660c53673628ab56ff
Pete Holiday <pete.holiday@gmail.com>
2014-01-17 01:42:01 -0500

    League create and update moved to rails.

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,21 +1,27 @@
 class League
-  include Mongoid::Document
+  include Mongoid::Document      
   field :name
   field :age_division
   field :season
   field :sport
-  field :start_date, type: Date
-  field :end_date, type: Date
+  field :start_date, type: Date, default: 5.weeks.from_now.to_date
+  field :end_date, type: Date, default: 15.weeks.from_now.to_date
   field :needs_standings_update, type: Boolean
   field :player_limit, type: Hash, default: {}
   field :price, type: Integer
-  field :registration_open, type: Date
-  field :registration_close, type: Date
+  field :registration_open, type: Date, default: 2.weeks.from_now.to_date
+  field :registration_close, type: Date, default: 4.weeks.from_now.to_date
   field :description, type: String
 
   has_many :teams, order: {'stats.rank' => :asc}
   has_many :registrations
   has_and_belongs_to_many :commissioners, class_name: "User", foreign_key: :commissioner_ids, inverse_of: nil
+
+  validates :name, :presence => true
+  validates :price, :numericality => { integer_only: true, greater_than: 0, less_than: 100, allow_blank: false  }
+  validates :age_division, :inclusion => { in: %w(adult juniors) }
+  validates :season, :inclusion => { in: %w(fall winter spring summer saturday) }
+  validates :sport, :inclusion => { in: %w(ultimate goaltimate) }
 
   scope :past,    -> { where(:end_date.lt => Date.today).order_by(start_date: :desc) }
   scope :future,  -> { where(:registration_open.gt => Date.today).order_by(start_date: :desc) }
--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module Platinum
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use Bootstrap style error styling
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
   end
 end
 
