b405fe2a147b662b74c1c19279f85788a7d77c1c
Pete Holiday <pete.holiday@gmail.com>
2014-02-09 11:13:04 -0500

    Minor UI tweaks for SNL 2014.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,15 @@
 class DashboardController < ApplicationController
+  def homepage
+    @leagues_registering = League.current.to_a.select{|l| l.registration_open? }
+    @leagues_running = League.current.to_a.select{|l| l.started? }
+    @your_teams = []
 
+    if current_user
+      team_ids = League.current.collect(&:team_ids).flatten & current_user[:teams]
+
+      team_ids.each do |tid|
+        @your_teams << Team.find(tid)
+      end
+    end
+  end
 end
