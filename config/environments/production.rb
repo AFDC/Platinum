ec7a2775a2af1e3b787ec907612f12eda2b3125a
Pete Holiday <pete.holiday@gmail.com>
2014-09-05 14:26:55 -0400

    Make the app error out if required figaro env variables aren't found.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Platinum::Application.configure do
       :address   => "smtp.mandrillapp.com",
       :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
       :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => ENV['mandrill_user'],
-      :password  => ENV['mandrill_pass'], # SMTP password is any valid API key
+      :user_name => Figaro.env.mandrill_user!,
+      :password  => Figaro.env.mandrill_pass!, # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       :domain => 'leagues.afdc.com', # your domain to identify your server when connecting
   }
---
+++ config/initializers/figaro.rb
@@ -0,0 +1,1 @@
+Figaro.require_keys('secret_token', 'roster_csv_path')
