4a3a2016b7e8d6cb37b5fe60375d54d7167ea330
Pete Holiday <pete.holiday@gmail.com>
2018-03-03 16:36:10 -0500

    MailChimpWorker should not unsubscribe users  - Refactor to leave subscribed users on the list even if they don't check the subscribe box  - Still force a user to subscribed if they check the box

--- app/workers/mail_chimp_worker.rb
+++ app/workers/mail_chimp_worker.rb
@@ -2,23 +2,29 @@ class MailChimpWorker
   include Sidekiq::Worker
 
   def perform(user_id, subscribe)
-    user = User.find(user_id)
-    subscribe_to_mailchimp(ENV['mailchimp_list_id'], user, subscribe)
-  end
+    user   = User.find(user_id)
+    gibbon = Gibbon::Request.new
 
-  def subscribe_to_mailchimp(list_id, user, subscribe)
-    gibbon = Gibbon::Request.new(api_key: ENV['mailchimp_api_key'])
-
-    status = 'subscribed'
-    status = 'unsubscribed' unless subscribe
-
-    gibbon.lists(list_id).members(user.email_md5).upsert(body: {
+    subscribe_body = {
       email_address: user.email_address,
-      status: status,
       merge_fields: { 
         FNAME: user.firstname,
         LNAME: user.lastname
       }
-     })
+    }
+
+    if subscribe
+      # We want to (re)subscribe the user regardless of their current status if they check the box
+      subscribe_body['status'] = 'subscribed'
+    else
+      # We want to make sure they're in our MailChimp list, but we don't want to unsub them if they're already subscribed
+      subscribe_body['status_if_new'] = 'unsubscribed'
+    end
+
+    gibbon.lists(list_id).members(user.email_md5).upsert(subscribe_body)
+  end
+
+  def list_id
+    ENV['mailchimp_list_id']
   end
 end
---
+++ config/initializers/gibbon.rb
@@ -0,0 +1,5 @@
+Gibbon::Request.api_key = ENV["mailchimp_api_key"]
+Gibbon::Request.timeout = 15
+Gibbon::Request.open_timeout = 15
+Gibbon::Request.symbolize_keys = false
+Gibbon::Request.debug = false
