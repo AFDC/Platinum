29ca51efe6ab5cb440e512e399910e52da5c5347
Pete Holiday <pete.holiday@gmail.com>
2018-04-07 09:53:47 -0400

    Update restore script.

--- mongo_import/restore_mongo.sh
+++ mongo_import/restore_mongo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tar -xzf platypus_dump.tar.gz
 docker cp dump/Platypus platinum_mongodb_1:/var/mongo_backup/
-MSYS_NO_PATHCONV=1 docker exec platinum_mongodb_1 mongorestore --drop -d Platypus /var/mongo_backup
+MSYS_NO_PATHCONV=1 docker exec platinum_mongodb_1 mongorestore --drop -d Platypus /var/mongo_backup/Platypus
 rm -rf dump
