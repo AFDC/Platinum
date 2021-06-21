a9d546dbd06e2620d2eed3445b44ea40c851047d
Pete Holiday <pete.holiday@gmail.com>
2021-06-21 01:34:32 -0400

    Remove querycache instruction from sidekiq initializer.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,3 @@ schedule_file = "config/schedule.yml"
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
-
-if Sidekiq.server?
-  Mongoid::QueryCache.enabled = false
-end
