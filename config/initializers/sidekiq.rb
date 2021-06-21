a52c08713d363b25784aaea238bc48c1c45c3c71
Pete Holiday <pete.holiday@gmail.com>
2021-06-21 01:04:18 -0400

    Turn off mongoid cache in Sidekiq server.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,7 @@ schedule_file = "config/schedule.yml"
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
+if Sidekiq.server?
+  Mongoid::QueryCache.enabled = false
+end
