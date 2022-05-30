cddc769b290c870cfc8797cd4f6c80339ba5f82b
Pete Holiday <pete.holiday@gmail.com>
2022-05-30 15:10:00 -0400

    Create default scope for pronouns
    Only some pronouns should be displayed in the dropdown by default.

--- app/models/pronoun.rb
+++ app/models/pronoun.rb
@@ -1,6 +1,8 @@
 class Pronoun
-	include Mongoid::Document
+  include Mongoid::Document
   field :label, type: String
   field :display, type: String
   field :default, type: Boolean
+
+  scope :defaults, where(default: true)
 end
