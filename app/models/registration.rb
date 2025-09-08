328ad11ae3fed2b5a9b538db2ce4202265f399f2
Pete Holiday <pete.holiday@gmail.com>
2025-09-08 16:43:27 -0400

    Creates method for generating a refund without cancelling a registration.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -136,6 +136,7 @@ class Registration
         return false if comped
 
         pt = payment_transactions.first
+        return false unless pt
 
         refund_amount = amount || pt.amount
         if refund_amount > pt.amount
@@ -153,6 +154,28 @@ class Registration
 
         self.status = 'canceled'
         save!
+    end
+
+    def generate_refund(amount = nil)
+        return false unless status == 'active'
+        return false if comped
+
+        pt = payment_transactions.first
+        return false unless pt
+
+        refund_amount = amount || pt.amount
+        if refund_amount > pt.amount
+            raise "Attempted to refund $#{refund_amount} but registration was only $#{pt.amount}"
+        end
+
+        result = Braintree::Transaction.refund(pt.transaction_id, amount: ("%.2f" % refund_amount))
+
+        unless result.success?
+            raise result.errors.first.message
+        end
+
+        pt.refunded_amount = refund_amount
+        pt.save
     end
 
     def is_expired?
